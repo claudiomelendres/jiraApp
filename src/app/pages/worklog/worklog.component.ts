d8c152b11c98de2617a8b084f0669790fdbec76f
Claudio Melendres <claudiomelendres@gmail.com>
2018-12-23 10:42:21 -0400

    first alert

--- src/app/pages/worklog/worklog.component.ts
+++ src/app/pages/worklog/worklog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { JiraService } from '../../services/service.index';
 
+import swal from 'sweetalert';
+
 @Component({
   selector: 'app-worklog',
   templateUrl: './worklog.component.html'
@@ -19,4 +21,10 @@ export class WorklogComponent implements OnInit {
     this.proyectos = this._jiraService.getProyectos();
   }
 
+<<<<<<< Updated upstream
+=======
+  showAlert() {
+    swal('Important', 'The logged time should not be 10% > of estimated time', 'warning');
+  }
+>>>>>>> Stashed changes
 }
