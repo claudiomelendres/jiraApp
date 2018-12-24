35f62597614d9e0526a5601b9b37b183b368a9fa
Claudio Melendres <claudiomelendres@gmail.com>
2018-12-23 23:36:28 -0400

    resolve chart

--- src/app/pages/charts/charts.component.ts
+++ src/app/pages/charts/charts.component.ts
@@ -18,14 +18,53 @@ export class ChartsComponent implements OnInit {
   ngOnInit() {
   }
 
+  // tslint:disable-next-line:member-ordering
   public lineChartOptions: any = {
     responsive: true
   };
-
-
+  // tslint:disable-next-line:member-ordering
+  public lineChartColors: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  // tslint:disable-next-line:member-ordering
   public lineChartLegend: boolean = true;
+  // tslint:disable-next-line:member-ordering
   public lineChartType: string = 'line';
 
+  public randomize(): void {
+    // tslint:disable-next-line:prefer-const
+    let _lineChartData: Array<any> = new Array(this.lineChartData.length);
+    for (let i = 0; i < this.lineChartData.length; i++) {
+      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+      }
+    }
+    this.lineChartData = _lineChartData;
+  }
 
   // events
   public chartClicked(e: any): void {
